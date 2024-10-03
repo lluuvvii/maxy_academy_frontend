799eb045015e282be65d6884304a81f16d2b2a19
lluuvvii <kurniawanluvi64@gmail.com>
2024-10-03 22:20:30 +0700

    update description of information about me

--- day38/my-personal-web/app/screens/AboutScreen.tsx
+++ day38/my-personal-web/app/screens/AboutScreen.tsx
@@ -6,7 +6,7 @@ const AboutScreen = () => {
     <View flex padding-20>
       <Text text40>About Me</Text>
       <Text text70 marginT-10>
-        Hi, I'm [Your Name], a passionate developer with expertise in web and mobile development.
+        Hi, I am a full-stack developer from Indonesia and have a dream to become a software engineer or frontend engineer, currently I am pursuing a bachelor's degree at PGRI Ronggolawe University in Indonesia and I am also freelancing as a frontend web developer and also have a hobby of exploring things in the world of IT and programming, to relax of course I play some games
       </Text>
     </View>
   );
